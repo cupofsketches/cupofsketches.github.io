1cf1a8d5745218c4d6ac5b80a3b339709dd6c7b0
Matheus Arruda <matheusarruda@saae.sp.gov.br>
2025-01-09 13:28:55 -0300

    add journeyCards.js

---
+++ card-status/seasons/journeyCards.js
@@ -0,0 +1,242 @@
+export const currentCollectionJourney = [
+    {
+        id: 'vacation',
+        name: 'Vacation',
+        legend: 'Vacation Cards',
+        cards: [
+            { name: 'Suitcase', isGold: false },
+            { name: 'Phone Booth', isGold: false },
+            { name: 'Train', isGold: false },
+            { name: 'Backpack', isGold: false },
+            { name: 'Camera', isGold: false },
+            { name: 'Passport', isGold: false },
+            { name: 'Map', isGold: false },
+            { name: 'Automat', isGold: false },
+            { name: 'Tickets', isGold: false },
+        ],
+    },
+    {
+        id: 'sightseeing',
+        name: 'Sightseeing',
+        legend: 'Sightseeing Cards',
+        cards: [
+            { name: 'Ruins', isGold: false },
+            { name: 'Arch', isGold: false },
+            { name: 'Souvenirs', isGold: false },
+            { name: 'Pedestal', isGold: false },
+            { name: 'Travel Journal', isGold: false },
+            { name: 'Display Case', isGold: false },
+            { name: 'Guidebook', isGold: false },
+            { name: 'Cable Car', isGold: false },
+            { name: 'Museum Ticket', isGold: false },
+        ],
+    },
+    {
+        id: 'japan',
+        name: 'Japan',
+        legend: 'Japan Cards',
+        cards: [
+            { name: 'Lucky Cat', isGold: false },
+            { name: 'Koi Fish', isGold: false },
+            { name: 'Cherry Blossom', isGold: false },
+            { name: 'Teapot', isGold: false },
+            { name: 'Old Slippers', isGold: false },
+            { name: 'Sushi', isGold: false },
+            { name: 'Chopsticks', isGold: false },
+            { name: 'Hand Fan', isGold: false },
+            { name: 'Old Gate', isGold: false },
+        ],
+    },
+    {
+        id: 'carnival',
+        name: 'Carnival',
+        legend: 'Carnival Cards',
+        cards: [
+            { name: 'Carousel', isGold: false },
+            { name: 'Circus Tent', isGold: false },
+            { name: 'Bumper Car', isGold: false },
+            { name: 'Clown Horn', isGold: false },
+            { name: 'Mask', isGold: false },
+            { name: 'Ferris Wheel', isGold: false },
+            { name: 'Ballon Dog', isGold: false },
+            { name: 'Teddy Bear', isGold: false },
+            { name: 'Hammer', isGold: false },
+        ],
+    },
+    {
+        id: 'camping',
+        name: 'Camping',
+        legend: 'Camping Cards',
+        cards: [
+            { name: 'Caravan', isGold: false },
+            { name: 'Tent', isGold: false },
+            { name: 'Marshmallow', isGold: false },
+            { name: 'Boots', isGold: false },
+            { name: 'Matches', isGold: false },
+            { name: 'Flashlight', isGold: false },
+            { name: 'Campfire', isGold: false },
+            { name: 'Saucepan', isGold: false },
+            { name: 'Log', isGold: false },
+        ],
+    },
+    {
+        id: 'discovery',
+        name: 'Discovery',
+        legend: 'Discovery Cards',
+        cards: [
+            { name: 'Drill', isGold: false },
+            { name: 'Own Statue', isGold: false },
+            { name: 'Mosaic', isGold: false },
+            { name: 'Pickaxe', isGold: false },
+            { name: 'Torch', isGold: false },
+            { name: 'Dynamite', isGold: false },
+            { name: 'Crate', isGold: false },
+            { name: 'Wheelbarrow', isGold: false },
+            { name: 'Hard Hat', isGold: false },
+        ],
+    },
+    {
+        id: 'underwater',
+        name: 'Underwater',
+        legend: 'Underwater Cards',
+        cards: [
+            { name: 'Turtle', isGold: false },
+            { name: 'Diving Suit', isGold: false },
+            { name: 'Flippers', isGold: false },
+            { name: 'Treasure', isGold: false },
+            { name: 'Anchor', isGold: false },
+            { name: 'Starfish', isGold: false },
+            { name: 'Snorkel', isGold: false },
+            { name: 'Seashell', isGold: false },
+            { name: 'Submarine', isGold: true }, // Gold card
+        ],
+    },
+    {
+        id: 'nature',
+        name: 'Nature',
+        legend: 'Nature Cards',
+        cards: [
+            { name: 'Mushroom', isGold: false },
+            { name: 'Dolphin', isGold: false },
+            { name: 'Oak Tree', isGold: false },
+            { name: 'Snowflake', isGold: false },
+            { name: 'Woodpecker', isGold: false },
+            { name: 'Bird Nest', isGold: false },
+            { name: 'Dandelion', isGold: false },
+            { name: 'Squirrel', isGold: false },
+            { name: 'Birdwatching', isGold: true }, // Gold card
+        ],
+    },
+    {
+        id: 'bazaar',
+        name: 'Bazzar',
+        legend: 'Bazzar Cards',
+        cards: [
+            { name: 'Spices', isGold: false },
+            { name: 'Porcelain Plate', isGold: false },
+            { name: 'Oud', isGold: false },
+            { name: 'Carpet', isGold: false },
+            { name: 'Old Coffepot', isGold: false },
+            { name: 'Old Scale', isGold: false },
+            { name: 'Hanging Lamp', isGold: false },
+            { name: 'Cashmere', isGold: false },
+            { name: 'Fortune Telling', isGold: true }, // Gold card
+        ],
+    },
+    {
+        id: 'sailing',
+        name: 'Sailing',
+        legend: 'Sailing Cards',
+        cards: [
+            { name: 'Canoe', isGold: false },
+            { name: "Ship Wheel", isGold: false },
+            { name: 'Lighthouse', isGold: false },
+            { name: 'Fishing Rod', isGold: false },
+            { name: 'Captain\'s Hat', isGold: false },
+            { name: 'Monocular', isGold: false },
+            { name: 'Compass', isGold: false },
+            { name: 'Ahoy!', isGold: true }, // Gold card
+            { name: 'Set Sail!', isGold: true }, // Gold card
+        ],
+    },
+    {
+        id: 'ancient',
+        name: 'Ancient',
+        legend: 'Ancient Cards',
+        cards: [
+            { name: 'Pyramide', isGold: false },
+            { name: 'Sphinx', isGold: false },
+            { name: 'Brooch', isGold: false },
+            { name: 'Feather Fan', isGold: false },
+            { name: 'Magic Lamp', isGold: false },
+            { name: 'Obelisk', isGold: false },
+            { name: 'Crook and Flail', isGold: false },
+            { name: 'Ancient Writing!', isGold: true }, // Gold card
+            { name: 'Hide and Seek!', isGold: true }, // Gold card
+        ],
+    },
+    {
+        id: 'polar',
+        name: 'Polar',
+        legend: 'Polar Cards',
+        cards: [
+            { name: 'Iceberg', isGold: false },
+            { name: 'Penguin', isGold: false },
+            { name: 'Blanket', isGold: false },
+            { name: 'Snow Shoes', isGold: false },
+            { name: 'Holly', isGold: false },
+            { name: 'Gift Sack', isGold: false },
+            { name: 'Snowballs', isGold: false },
+            { name: 'Getting Warm!', isGold: true }, // Gold card
+            { name: 'Stack the Blocks!', isGold: true }, // Gold card
+        ],
+    },
+    {
+        id: 'safari',
+        name: 'Safari',
+        legend: 'Safari Cards',
+        cards: [
+            { name: 'Acacia Tree', isGold: false },
+            { name: 'Zebra', isGold: false },
+            { name: 'Totem', isGold: false },
+            { name: 'Tribal Mask', isGold: false },
+            { name: 'Giraffe', isGold: false },
+            { name: 'Bongo Drums', isGold: false },
+            { name: "Snap the Shot!", isGold: true }, // Gold card
+            { name: 'Track the Tracks!', isGold: true }, // Gold card
+            { name: 'Treats', isGold: true }, // Gold card
+        ],
+    },
+    {
+        id: 'exploration',
+        name: 'Exploration',
+        legend: 'Exploration Cards',
+        cards: [
+            { name: 'Mission Patch', isGold: false },
+            { name: 'Greenhouse', isGold: false },
+            { name: 'Spacesuit', isGold: false },
+            { name: 'Space Prob', isGold: false },
+            { name: 'Solar Panel', isGold: false },
+            { name: 'Planetarium', isGold: false },
+            { name: 'Moon Landing!', isGold: true }, // Gold card
+            { name: "No Gravity!", isGold: true }, // Gold card
+            { name: 'On Duty!', isGold: true }, // Gold card
+        ],
+    },
+    {
+        id: 'ocean',
+        name: 'Ocean',
+        legend: 'Ocean Cards',
+        cards: [
+            { name: 'Jellyfish', isGold: false },
+            { name: 'Shipwreck', isGold: false },
+            { name: 'Conch shell', isGold: false },
+            { name: 'Seahorse', isGold: false },
+            { name: 'Sea Anemone', isGold: false },
+            { name: 'Stingray', isGold: false },
+            { name: 'Hello!', isGold: true }, // Gold card
+            { name: 'Diving!', isGold: true }, // Gold card
+            { name: 'Catching Waves!', isGold: true }, // Gold card
+        ],
+    },
+];

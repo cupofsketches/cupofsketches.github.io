0ca6690c4e2932590e3d0cf78f89c9b09214345c
Cup of Sketches <cupofsketches@gmail.com>
2025-01-09 15:28:48 -0300

    Reference to Journey Collection added

--- card-status/updated/cardsData.js
+++ card-status/updated/cardsData.js
@@ -1,7 +1,8 @@
-export const season = 'winter'; // Set this dynamically based on your logic, e.g., using the current date.
+export const season = 'journey'; // Set this dynamically based on your logic, e.g., using the current date.
 
 import { currentCollectionFall } from '../seasons/fallCards.js';
 import { currentCollectionWinter } from '../seasons/winterCards.js';
+import { currentCollectionJourney } from '../seasons/journeyCards.js';
 
 let currentCollection;
 
@@ -11,13 +12,10 @@ export function loadCollection() {
         currentCollection = currentCollectionFall; // Import fall cards
     } else if (season === 'winter') {
         currentCollection = currentCollectionWinter; // Import fall cards
-    } /*else if (season === 'summer') {
-        currentCollection = currentCollectionSummer; // Import fall cards
-        return currentCollection; // Return the loaded collection directly
-    } else if (season === 'spring') {
-        currentCollection = currentCollectionSpring; // Import fall cards
-        return currentCollection; // Return the loaded collection directly
-    }*/
+    } else if (season === 'journey') {
+        currentCollection = currentCollectionJourney; // Import fall cards
+    }
+
     return currentCollection; // Or handle the case when season doesn't match
 }
 

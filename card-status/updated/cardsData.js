bd8540766ddcf836be657c89227f4f369795355c
Cup of Sketches <cupofsketches@gmail.com>
2025-07-06 19:09:38 -0500

    Files Updated for Hobby Collection

--- card-status/updated/cardsData.js
+++ card-status/updated/cardsData.js
@@ -1,10 +1,11 @@
-export const season = 'space'; // Set this dynamically based on your logic, e.g., using the current date.
+export const season = 'hobby'; // Set this dynamically based on your logic, e.g., using the current date.
 
 import { currentCollectionFall } from '../seasons/fallCards.js';
 import { currentCollectionWinter } from '../seasons/winterCards.js';
 import { currentCollectionJourney } from '../seasons/journeyCards.js';
 import { currentCollectionNature } from '../seasons/natureCards.js';
 import { currentCollectionSpace } from '../seasons/spaceCards.js';
+import { currentCollectionHobby } from '../seasons/hobbyCards.js';
 
 let currentCollection;
 
@@ -20,7 +21,10 @@ export function loadCollection() {
         currentCollection = currentCollectionNature; // Import nature cards
     } else if (season === 'space') {
         currentCollection = currentCollectionSpace; // Import space cards
+    } else if (season === 'hobby') {
+        currentCollection = currentCollectionHobby;
     }
+
     console.log(currentCollection);
     return currentCollection; // Or handle the case when season doesn't match
 

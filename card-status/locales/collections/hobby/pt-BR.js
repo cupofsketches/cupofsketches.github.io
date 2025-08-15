b5b1a660b539b48568f5126cc3bdeee301e73b33
Cup of Sketches <cupofsketches@gmail.com>
2025-08-15 15:33:06 -0500

    dd complete Portuguese hobby collection with all 15 decks and card translations

--- card-status/locales/collections/hobby/pt-BR.js
+++ card-status/locales/collections/hobby/pt-BR.js
@@ -1,1 +1,622 @@
+// =========================================================================
+// locales/collections/hobby/pt-BR.js - Portuguese hobby collection data
+// Collaboration by u/carloslaso
+// =========================================================================
 
+export const hobbyCollection = [
+    {
+        "id": "music-studio",
+        "name": "Estúdio de Música",
+        "legend": "Estúdio de Música",
+        "cards": [
+            {
+                "id": "tape-recorder",
+                "name": "Gravador",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "tuba",
+                "name": "Tuba",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "xylophone",
+                "name": "Xilofone",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "studio-foam",
+                "name": "Espuma de Estúdio",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "guitar",
+                "name": "Violão",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "turntable",
+                "name": "Toca-discos",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "drums",
+                "name": "Bateria",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "cymbal",
+                "name": "Prato",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "violin",
+                "name": "Violino",
+                "stars": 2,
+                "isGold": false
+            }
+        ]
+    },
+    {
+        "id": "vehicles",
+        "name": "Veículos",
+        "legend": "Veículos",
+        "cards": [
+            {
+                "id": "yacht",
+                "name": "Iate",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "car",
+                "name": "Carro",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "helmet",
+                "name": "Capacete",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "wheel",
+                "name": "Roda",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "lug-wrench",
+                "name": "Chave de Roda",
+                "stars": 1,
+                "isGold": false
+            }
+        ]
+    },
+    {
+        "id": "factory",
+        "name": "Fábrica",
+        "legend": "Fábrica",
+        "cards": [
+            {
+                "id": "conveyor-belt",
+                "name": "Esteira",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "robot-arm",
+                "name": "Braço Robótico",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "control-panel",
+                "name": "Painel de Controle",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "steam-pipe",
+                "name": "Tubo de Vapor",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "gears",
+                "name": "Engrenagens",
+                "stars": 1,
+                "isGold": false
+            }
+        ]
+    },
+    {
+        "id": "jewels",
+        "name": "Joias",
+        "legend": "Joias",
+        "cards": [
+            {
+                "id": "diamond",
+                "name": "Diamante",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "ruby",
+                "name": "Rubi",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "emerald",
+                "name": "Esmeralda",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "sapphire",
+                "name": "Safira",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "pearl",
+                "name": "Pérola",
+                "stars": 1,
+                "isGold": false
+            }
+        ]
+    },
+    {
+        "id": "glass",
+        "name": "Vidro",
+        "legend": "Vidro",
+        "cards": [
+            {
+                "id": "wine-glass",
+                "name": "Taça de Vinho",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "vase",
+                "name": "Vaso",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "mirror",
+                "name": "Espelho",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "lamp",
+                "name": "Lâmpada",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "bottle",
+                "name": "Garrafa",
+                "stars": 1,
+                "isGold": false
+            }
+        ]
+    },
+    {
+        "id": "street",
+        "name": "Rua",
+        "legend": "Rua",
+        "cards": [
+            {
+                "id": "streetlight",
+                "name": "Poste de Luz",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "bench",
+                "name": "Banco",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "trash-can",
+                "name": "Lixeira",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "fire-hydrant",
+                "name": "Hidrante",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "mailbox",
+                "name": "Caixa de Correio",
+                "stars": 1,
+                "isGold": false
+            }
+        ]
+    },
+    {
+        "id": "fantasy",
+        "name": "Fantasia",
+        "legend": "Fantasia",
+        "cards": [
+            {
+                "id": "dragon",
+                "name": "Dragão",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "wizard",
+                "name": "Mago",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "castle",
+                "name": "Castelo",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "sword",
+                "name": "Espada",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "shield",
+                "name": "Escudo",
+                "stars": 1,
+                "isGold": false
+            }
+        ]
+    },
+    {
+        "id": "paper-craft",
+        "name": "Artesanato de Papel",
+        "legend": "Artesanato de Papel",
+        "cards": [
+            {
+                "id": "origami-crane",
+                "name": "Grou de Origami",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "paper-airplane",
+                "name": "Avião de Papel",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "paper-flower",
+                "name": "Flor de Papel",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "paper-boat",
+                "name": "Barco de Papel",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "paper-fan",
+                "name": "Leque de Papel",
+                "stars": 1,
+                "isGold": false
+            }
+        ]
+    },
+    {
+        "id": "styles",
+        "name": "Estilos",
+        "legend": "Estilos",
+        "cards": [
+            {
+                "id": "vintage",
+                "name": "Vintage",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "modern",
+                "name": "Moderno",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "classic",
+                "name": "Clássico",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "elegant",
+                "name": "Elegante",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "casual",
+                "name": "Casual",
+                "stars": 1,
+                "isGold": false
+            }
+        ]
+    },
+    {
+        "id": "games",
+        "name": "Jogos",
+        "legend": "Jogos",
+        "cards": [
+            {
+                "id": "chess-piece",
+                "name": "Peça de Xadrez",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "dice",
+                "name": "Dados",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "playing-card",
+                "name": "Carta de Baralho",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "puzzle-piece",
+                "name": "Peça de Quebra-Cabeça",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "game-controller",
+                "name": "Controle de Jogo",
+                "stars": 1,
+                "isGold": false
+            }
+        ]
+    },
+    {
+        "id": "sand-craft",
+        "name": "Artesanato de Areia",
+        "legend": "Artesanato de Areia",
+        "cards": [
+            {
+                "id": "sandcastle",
+                "name": "Castelo de Areia",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "sand-sculpture",
+                "name": "Escultura de Areia",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "sand-art",
+                "name": "Arte de Areia",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "sand-bottle",
+                "name": "Garrafa de Areia",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "sand-painting",
+                "name": "Pintura de Areia",
+                "stars": 1,
+                "isGold": false
+            }
+        ]
+    },
+    {
+        "id": "culinary",
+        "name": "Culinária",
+        "legend": "Culinária",
+        "cards": [
+            {
+                "id": "chef-hat",
+                "name": "Chapéu de Chef",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "spatula",
+                "name": "Espátula",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "whisk",
+                "name": "Batedor",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "rolling-pin",
+                "name": "Rolo de Massa",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "measuring-cup",
+                "name": "Copo Medidor",
+                "stars": 1,
+                "isGold": false
+            }
+        ]
+    },
+    {
+        "id": "balloon-art",
+        "name": "Arte de Balões",
+        "legend": "Arte de Balões",
+        "cards": [
+            {
+                "id": "balloon-animal",
+                "name": "Animal de Balão",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "balloon-flower",
+                "name": "Flor de Balão",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "balloon-sword",
+                "name": "Espada de Balão",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "balloon-heart",
+                "name": "Coração de Balão",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "balloon-hat",
+                "name": "Chapéu de Balão",
+                "stars": 1,
+                "isGold": false
+            }
+        ]
+    },
+    {
+        "id": "toys",
+        "name": "Brinquedos",
+        "legend": "Brinquedos",
+        "cards": [
+            {
+                "id": "teddy-bear",
+                "name": "Ursinho de Pelúcia",
+                "stars": 1,
+                "isGold": false
+            },
+            {
+                "id": "building-blocks",
+                "name": "Blocos de Construção",
+                "stars": 4,
+                "isGold": false
+            },
+            {
+                "id": "drone",
+                "name": "Drone",
+                "stars": 5,
+                "isGold": false
+            },
+            {
+                "id": "unicorn",
+                "name": "Unicórnio",
+                "stars": 5,
+                "isGold": false
+            },
+            {
+                "id": "propeller",
+                "name": "Hélice",
+                "stars": 5,
+                "isGold": true
+            },
+            {
+                "id": "me-time",
+                "name": "Meu Tempo!",
+                "stars": 5,
+                "isGold": true
+            },
+            {
+                "id": "chasing",
+                "name": "Perseguição",
+                "stars": 5,
+                "isGold": true
+            }
+        ]
+    },
+    {
+        "id": "renaissance",
+        "name": "Renascimento",
+        "legend": "Renascimento",
+        "cards": [
+            {
+                "id": "sketchbook",
+                "name": "Caderno de Esboços",
+                "stars": 4,
+                "isGold": false
+            },
+            {
+                "id": "wing-model",
+                "name": "Modelo de Asa",
+                "stars": 4,
+                "isGold": false
+            },
+            {
+                "id": "gear-set",
+                "name": "Conjunto de Engrenagens",
+                "stars": 4,
+                "isGold": false
+            },
+            {
+                "id": "bronze-casting",
+                "name": "Fundição de Bronze",
+                "stars": 4,
+                "isGold": false
+            },
+            {
+                "id": "cipher-disc",
+                "name": "Disco de Cifra",
+                "stars": 5,
+                "isGold": false
+            },
+            {
+                "id": "ship-model",
+                "name": "Modelo de Navio",
+                "stars": 5,
+                "isGold": false
+            },
+            {
+                "id": "parachute",
+                "name": "Paraquedas",
+                "stars": 5,
+                "isGold": true
+            },
+            {
+                "id": "ornithopter",
+                "name": "Ornitóptero",
+                "stars": 5,
+                "isGold": true
+            },
+            {
+                "id": "clockwork",
+                "name": "Mecanismo de Relógio",
+                "stars": 5,
+                "isGold": true
+            }
+        ]
+    }
+];
